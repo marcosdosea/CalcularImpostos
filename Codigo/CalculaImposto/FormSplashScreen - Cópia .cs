e2d1738553ce71061ab2c685c4d168deff473a43
Magda Amaral <magdaamaral2014@yahoo.com.br>
2020-06-11 20:04:03 -0300

    Criação Splash Configura caminho de dropbox máquina.

---
+++ Codigo/CalculaImposto/FormSplashScreen - Cópia .cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculaImposto
+{
+    public partial class FormSplashScreen : Form
+    {
+        public FormSplashScreen()
+        {
+            InitializeComponent();
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            panel2.Width += 3;
+            if (panel2.Width >= 790)
+            {
+                timer1.Stop();
+
+                FrmCalculaImposto frm = new FrmCalculaImposto();
+
+                frm.Show();
+       
+                this.Hide();
+
+            }
+        }
+    }
+}
